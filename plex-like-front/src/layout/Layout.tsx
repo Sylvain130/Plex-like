033e3379ded1027f4798ad138e247ebd07cade79
sylvanouch <sylvain-joubert@hotmail.fr>
2022-03-30 18:29:11 +0200

    correction Layout bug

---
+++ plex-like-front/src/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Topbar from "./components/topbar/Topbar";
+
+interface Ilayout {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Ilayout): JSX.Element => {
+  return (
+    <>
+      <Topbar />
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
