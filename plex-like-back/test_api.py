f1f48edc8a9789dcd4b8c17da8de66f765333d9c
sylvanouch <sylvain-joubert@hotmail.fr>
2022-04-27 14:43:46 +0200

    Add init db

--- plex-like-back/test_api.py
+++ plex-like-back/test_api.py
@@ -14,35 +14,28 @@ def app():
     })
 
     # other setup can go here
-
     yield app
-
     # clean up / reset resources here
-
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
-
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
-
 
 def test_request_example(client):
     response = client.get("/")
     assert b'test' in response.data
 
 def test_register(client):
-    response = client.post("/register", data=json.dumps({"name":"Yasoob","password":"strongpassword"}),content_type='application/json',)
+    response = client.post("/register", data=json.dumps({"email":"sylvain-joubert@hotmail.fr","password":"password"}),content_type='application/json',)
     assert response.status_code == 200
     assert response.data == b'{"message":"registeration successfully"}\n'
 
-
-
 def test_login(client):
-    response = client.post("/login", headers={"Authorization": "Basic {}".format(base64.b64encode(b'Yasoob:strongpassword').decode('utf-8'))})
+    response = client.post("/login", headers={"Authorization": "Basic {}".format(base64.b64encode(b'sylvain-joubert@hotmail.fr:password').decode('utf-8'))})
     assert response.status_code == 200
     assert response.data[0:9] == b'{"token":'
 
