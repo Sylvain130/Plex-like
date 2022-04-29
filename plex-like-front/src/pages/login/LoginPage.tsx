0126b0eab7ae3505c7ea960a0ef4fb8a9fda0852
sylvanouch <sylvain-joubert@hotmail.fr>
2022-04-29 18:11:59 +0200

    finished login and register (front + back)

--- plex-like-front/src/pages/login/LoginPage.tsx
+++ plex-like-front/src/pages/login/LoginPage.tsx
@@ -1,5 +1,6 @@
-import { Box, TextField, Input, Link } from "@mui/material";
+import { Box, TextField, Input, Link, Typography } from "@mui/material";
 import { SxProps } from "@mui/system";
+import { useState } from "react";
 import { FieldErrors, useForm } from "react-hook-form";
 import base64 from "react-native-base64";
 import { useNavigate } from "react-router-dom";
@@ -35,6 +36,11 @@ const styleLoginPage: SxProps = {
   },
 };
 
+const styleTypo: SxProps = {
+  color: "#f44336",
+  fontSize: "0.75rem",
+};
+
 const errorMessageEmail = (errors: FieldErrors) => {
   if (errors?.Email?.type === "required") {
     return "Email requis";
@@ -44,6 +50,9 @@ const errorMessageEmail = (errors: FieldErrors) => {
 };
 
 const LoginPage = (): JSX.Element => {
+  const [errorUser, setErrorUser] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -54,19 +63,26 @@ const LoginPage = (): JSX.Element => {
 
   const onSubmit = handleSubmit((data) => onSubmitClick(data));
   const onSubmitClick = (data: FormValues) => {
-    console.log("You pressed login");
     const headers = new Headers({
       Authorization: "Basic " + base64.encode(data.email + ":" + data.password),
     });
     fetch("http://localhost:5000/login", {
       method: "post",
       headers: headers,
-    })
-      .then((r) => r.json())
-      .then((token) => {
-        console.log(token);
+    }).then(async (response) => {
+      const r = await response;
+      const message = await r.json();
+      if (r.status !== 200) {
+        if (message.message === "user does not exist") {
+          setErrorUser(true);
+        }
+        if (message.message === "invalid password") {
+          setErrorPassword(true);
+        }
+      } else {
         navigate("/films");
-      });
+      }
+    });
   };
 
   return (
@@ -75,8 +91,7 @@ const LoginPage = (): JSX.Element => {
       <Box
         component="form"
         sx={styleForm}
-        noValidate
-        autoComplete="off"
+        autoComplete="on"
         onSubmit={onSubmit}
       >
         <TextField
@@ -84,22 +99,37 @@ const LoginPage = (): JSX.Element => {
           {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
           placeholder="Email"
           helperText={errorMessageEmail(errors)}
+          onChange={() => setErrorUser(false)}
         />
+        {errorUser ? (
+          <Typography sx={styleTypo}>
+            {" "}
+            Cet email n'est pas associé à un compte
+          </Typography>
+        ) : (
+          <></>
+        )}
 
         <TextField
           sx={styleInput}
           {...register("password", { required: true })}
+          type="password"
           placeholder="Mot de passe"
           helperText={
             errors?.password?.type === "required" && "Mot de passe requis"
           }
+          onChange={() => setErrorPassword(false)}
         />
+        {errorPassword ? (
+          <Typography sx={styleTypo}> Le mot de passe est invalide</Typography>
+        ) : (
+          <></>
+        )}
 
         <Input sx={styleInput} type="submit" value="se connecter" />
       </Box>
       <Link href="/Register" align="center">
-        {" "}
-        Pas inscrit? L'inscription c'est ici{" "}
+        Pas inscrit? L'inscription c'est ici
       </Link>
     </Box>
   );
--- plex-like-front/src/pages/register/RegisterPage.tsx
+++ plex-like-front/src/pages/register/RegisterPage.tsx
@@ -1,5 +1,6 @@
-import { Box, TextField, Input } from "@mui/material";
+import { Box, TextField, Input, Typography } from "@mui/material";
 import { SxProps } from "@mui/system";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import base64 from "react-native-base64";
 import { useNavigate } from "react-router-dom";
@@ -10,72 +11,88 @@ type FormValues = {
   confirmPassword: string;
 };
 
+const styleHomePage: SxProps = {
+  width: "100%",
+  height: "100vh",
+
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+
+  "& > *": {
+    margin: "15px",
+    width: "30%",
+  },
+};
+
+const styleForm: SxProps = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const styleInput: SxProps = {
+  margin: "0.3rem",
+};
+
+const styleTypo: SxProps = {
+  color: "#f44336",
+  fontSize: "0.75rem",
+};
+
 const RegisterPage = (): JSX.Element => {
+  const [errorUser, setErrorUser] = useState(false);
+
   const {
     register,
     getValues,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>();
-  
+
   const navigate = useNavigate();
 
   const onSubmit = handleSubmit((data) => onSubmitClick(data));
-  const onSubmitClick = (data: FormValues) => {
-    console.log("You pressed register");
 
+  const onSubmitClick = (data: FormValues) => {
     const headers1 = new Headers({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     });
-    const body = JSON.stringify({ "email":`${data.email}`,"password":`${data.password}`});
+    const body = JSON.stringify({
+      email: `${data.email}`,
+      password: `${data.password}`,
+    });
 
     fetch("http://localhost:5000/register", {
       method: "post",
       headers: headers1,
       body: body,
     })
-      .then((r) => r.json())
-    
-    console.log("login");
-
-    const headers2 = new Headers({
-      Authorization: "Basic " + base64.encode(data.email + ":" + data.password),
-    });
-    fetch("http://localhost:5000/login", {
-      method: "post",
-      headers: headers2,
-    })
-      .then((r) => r.json())
-      .then((token) => {
-        console.log(token);
-        navigate("/films");
-      });
-  };
-
-  const styleHomePage: SxProps = {
-    width: "100%",
-    height: "100vh",
-
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-
-    "& > *": {
-      margin: "15px",
-      width: "30%",
-    },
-  };
-
-  const styleForm: SxProps = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const styleInput: SxProps = {
-    margin: "0.3rem",
+      .then(async (response) => {
+        const r = await response;
+        const message = await r.json();
+        if (r.status !== 200) {
+          if (message.message === "user already exist") {
+            setErrorUser(true);
+          }
+        } else {
+          if (message.message === "registeration successfully") {
+            const headers2 = new Headers({
+              Authorization:
+                "Basic " + base64.encode(data.email + ":" + data.password),
+            });
+            fetch("http://localhost:5000/login", {
+              method: "post",
+              headers: headers2,
+            }).then(() => {
+              navigate("/films");
+            });
+          }
+        }
+      })
+      .catch();
   };
 
   return (
@@ -83,8 +100,7 @@ const RegisterPage = (): JSX.Element => {
       <Box
         component="form"
         sx={styleForm}
-        noValidate
-        autoComplete="off"
+        autoComplete="on"
         onSubmit={onSubmit}
       >
         <TextField
@@ -96,19 +112,28 @@ const RegisterPage = (): JSX.Element => {
           placeholder="Email"
           helperText={errors?.email?.message}
           error={errors.email !== undefined}
+          onChange={() => setErrorUser(false)}
         />
 
+        {errorUser ? (
+          <Typography sx={styleTypo}>
+            {" "}
+            Cet email est déjà associé à un compte
+          </Typography>
+        ) : (
+          <></>
+        )}
+
         <TextField
-          type="password"
           sx={styleInput}
           {...register("password", { required: "Mot de passe requis" })}
+          type="password"
           placeholder="Mot de passe"
           helperText={errors?.password?.message}
           error={errors.password !== undefined}
         />
 
         <TextField
-          type="password"
           sx={styleInput}
           {...register("confirmPassword", {
             validate: {
@@ -119,6 +144,7 @@ const RegisterPage = (): JSX.Element => {
                 "Les mots de passe sont différents",
             },
           })}
+          type="password"
           placeholder="Confirmation mot de passe"
           helperText={errors?.confirmPassword?.message}
           error={errors.confirmPassword !== undefined}
